e366e19fbfa54b97e841eabd46b40fb260751283
Sam Johnson <samueljoh@gmail.com>
2023-10-30 16:28:11 -0600

    reverted some code that chatgpt got rid of

--- server.js
+++ server.js
@@ -24,11 +24,11 @@ const storage = multer.memoryStorage(); // <-- Use memory storage
 const upload = multer({ storage: storage });
 
 app.post("/analyze", upload.single("image"), async (req, res) => {
-  const imageBuffer = req.file.buffer; // <-- Get the file buffer
+  const imagePath = req.file.path;
   const apiUrl = "https://curanexa4.uc.r.appspot.com/analyze";
 
   const form = new FormData();
-  form.append("image", imageBuffer, { filename: "image.jpg" }); // <-- Append the buffer as a file
+  form.append("image", fs.createReadStream(imagePath));
 
   try {
     const response = await axios.post(apiUrl, form, {
@@ -37,11 +37,29 @@ app.post("/analyze", upload.single("image"), async (req, res) => {
       },
     });
 
-    // Return analysis results in response
-    res.json(response.data);
+    // Save analysis results to file
+    const analysisId = path.basename(imagePath, path.extname(imagePath));
+    const analysisPath = `analyses/${analysisId}.json`;
+    fs.writeFileSync(analysisPath, JSON.stringify(response.data));
+
+    // Return analysis identifier in response
+    res.json({ analysisId });
   } catch (error) {
     console.error(`Error: ${error}`);
     res.status(500).send(error.message);
+  }
+});
+
+app.get("/analysis/:analysisId", (req, res) => {
+  const analysisId = req.params.analysisId;
+  const analysisPath = `analyses/${analysisId}.json`;
+
+  // Retrieve analysis results from file
+  if (fs.existsSync(analysisPath)) {
+    const analysisResults = fs.readFileSync(analysisPath, "utf-8");
+    res.json(JSON.parse(analysisResults));
+  } else {
+    res.status(404).send("Analysis not found");
   }
 });
 
